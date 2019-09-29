345dabc062d552a763f55f60a0c7223711d8f3e1
oodennis20 <oodennis20@gmail.com>
2019-09-29 11:23:03 +0300

    add a toggle function to id card1,2 and 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
-$(document).ready(function(){
-  $("button").click(function(){
-    $("p").toggle();
-  });
+$("#one").click(function() {
+    $("#paragraph1").toggle(); 
+    $("#one").toggle(); 
+});
+ $("#paragraph1").click(function() {
+    $("#one").toggle();
+    $("#paragraph1").toggle();
+});
+$("#two").click(function() {
+    $("#paragraph2").toggle();
+    $("#two").toggle(); 
+});
+ $("#paragraph2").click(function() {
+    $("#two").toggle();
+    $("#paragraph").toggle();
+});
+$("#three").click(function() {
+    $("#paragraph3").toggle();
+    $("#three").toggle(); 
+});
+ $("#paragraph3").click(function() {
+    $("#three").toggle();
+    $("#paragraph3").toggle();
 });
