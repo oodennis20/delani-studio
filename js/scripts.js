5e5fda3227d3d20d71ce78cf1ce0c0c7a6133268
oodennis20 <oodennis20@gmail.com>
2019-09-29 23:02:31 +0300

    add a hover effect in class col-sm-3

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 $("#one").click(function() {
     $("#paragraph1").toggle(); 
-    $("#one").toggle(); 
+    $("#one").toggle(   ); 
 });
  $("#paragraph1").click(function() {
-    $("#one").toggle();
+    $("#one").toggle(show);
     $("#paragraph1").toggle();
 });
 $("#two").click(function() {
     $("#paragraph2").toggle();
-    $("#two").toggle(); 
+    $("#two").toggle(show); 
 });
  $("#paragraph2").click(function() {
     $("#two").toggle();
@@ -16,9 +16,16 @@ $("#two").click(function() {
 });
 $("#three").click(function() {
     $("#paragraph3").toggle();
-    $("#three").toggle(); 
+    $("#three").toggle(show); 
 });
  $("#paragraph3").click(function() {
     $("#three").toggle();
     $("#paragraph3").toggle();
 });
+$(document).ready(function() { 
+    $(".col-sm-3").hover(function() { 
+        $(this).css("background-color", "green"); 
+    }, function() { 
+        $(this).css("background-color", "yellow"); 
+    }); 
+}); 
